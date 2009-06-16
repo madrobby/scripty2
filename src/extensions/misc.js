c07e32457ef19446d39c0d087cb4774cc9a98bd4
Thomas Fuchs <thomas@fesch.at>
2009-06-16 19:30:10 +0200

    Add documentation for Number#tween

--- src/extensions/misc.js
+++ src/extensions/misc.js
@@ -10,6 +10,23 @@ Function.prototype.optionize = function(){
   return method;
 };
 
+/** section: scripty2 core
+ * class Number
+ *  
+ *  Extensions to the built-in `Number` object.
+**/
+
+/**
+ *  Number#tween(target, position) -> Number
+ *  - target (Number): tween target
+ *  - position (Number): position between 0 (start of tween) and (end of tween); can also be < 0 and > 1.
+ *
+ *  Returns the number that is a given percentage between this number and a target number.
+ *
+ *      (1).tween(2, 0.5) -> 1.5
+ *      (1).tween(2, 0) -> 1
+ *      (1).tween(2, 1) -> 2
+**/
 Number.prototype.tween = function(target, position){
   return this + (target-this) * position;
 };
