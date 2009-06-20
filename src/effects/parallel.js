0c2146654c1fa97c1bacfe0617d8e9ceb3b7f06f
Philipp Markovics <philipp@markovics.com>
2009-06-20 17:01:33 +0200

    pass empty object if no options given

--- src/effects/parallel.js
+++ src/effects/parallel.js
@@ -6,7 +6,7 @@
 s2.fx.Parallel = Class.create(s2.fx.Base, {
   initialize: function($super, effects, options) {
     this.effects = effects || [];
-    return $super(options);
+    return $super(options || {});
   },
 
   setup: function() {
