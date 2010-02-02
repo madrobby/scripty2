04ddc080d99109186851f2d3bb5d24d2b536c1a8
Thomas Fuchs <thomas@fesch.at>
2010-02-02 16:53:34 +0100

    update years in copyright statements

--- src/license.js
+++ src/license.js
@@ -1,6 +1,6 @@
 /*! 
  *  script.aculo.us version <%= SCRIPTY2_VERSION %>
- *  (c) 2005-2009 Thomas Fuchs
+ *  (c) 2005-2010 Thomas Fuchs
  *
  *  script.aculo.us is freely distributable under the terms of an MIT-style license.
  *----------------------------------------------------------------------------------*/
