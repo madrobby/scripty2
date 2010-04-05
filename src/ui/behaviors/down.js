8ad32468756626cb5974dab3dd8fba22acd67b38
Andrew Dupont <prototype@andrewdupont.net>
2010-04-05 00:43:02 -0500

    Make the `Down` behavior treat a keydown (with ENTER or SPACE) like a mousedown.

--- src/ui/behaviors/down.js
+++ src/ui/behaviors/down.js
@@ -1,10 +1,29 @@
 /** section: scripty2 ui
- *  class S2.UI.Behavior.Down < UI.Behavior
+ *  class S2.UI.Behavior.Down < S2.UI.Behavior
  *  
  *  Applies a down-state behavior. Adds a `ui-state-down` class to any 
  *  non-disabled element on mousedown, removing it on mouseup.
+ *
+ *  Also applies these behaviors on keydown when the key is the space bar or
+ *  the return key (since browsers differ on whether mousedown/up handlers fire
+ *  in this situation).
 **/
 S2.UI.Behavior.Down = Class.create(S2.UI.Behavior, {
+  _isRelevantKey: function(event) {
+    var code = event.keyCode;
+    return (code === Event.KEY_RETURN || code === Event.KEY_SPACE);
+  },
+  
+  onkeydown: function(event) {
+    if (!this._isRelevantKey(event)) return;
+    this.onmousedown(event);
+  },
+  
+  onkeyup: function(event) {
+    if (!this._isRelevantKey(event)) return;
+    this.onmouseup(event);
+  },
+  
   onmousedown: function(event) {
     this._down = true;
     if (this.element.hasClassName('ui-state-disabled')) return;
