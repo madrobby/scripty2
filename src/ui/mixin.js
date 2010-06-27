2eb329a143211edf5717c5198d95834ec4add9ed
Thomas Fuchs <thomas@fesch.at>
2010-06-27 11:52:49 +0200

    a few words on mixins

--- src/ui/mixin.js
+++ src/ui/mixin.js
@@ -1,6 +1,15 @@
-
 /** section: scripty2 ui
  * S2.UI.Mixin
+ *
+ * Reusable mixins for user interface elements.
+ *
+ * * [[S2.UI.Mixin.Configurable]] for hassle-free blending of 
+ *   default options with user-defined options. 
+ * * [[S2.UI.Mixin.Trackable]]
+ * * [[S2.UI.Mixin.Element]] provides convenience methods for widgets 
+ *   that map easily to a single element.
+ * * [[S2.UI.Mixin.Shim]] provides an implementation of a "shim" for Internet Explorer 6,
+ *   to avoid rendering z-order problems on that browser.
 **/
 S2.UI.Mixin = {};
 
